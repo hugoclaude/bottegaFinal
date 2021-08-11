7a273141719d930ee028ed9b9753cb8d34008f24
hugoclaude <hugocayeros@gmail.com>
2021-08-10 20:00:44 -0700

    build login/home elements

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,2 +1,42 @@
+import { urlencoded } from 'express';
 import React, { Component } from 'react';
-import Login from '../auth/login'
+import Login from '../auth/login';
+//import loginImg from '../../../static/assets/images/auth/login.jpn';
+
+export default class Auth extends component {
+    constructor(props) {
+        super(props);
+
+        this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+        this.handleUnsuccessfulAuth = this.handleUnsuccessfulAuth.bing(this);
+    }
+
+    handleSuccessfulAuth() {
+        this.props.handleSuccessfulLogin();
+        this.props.history.push('/');
+    }
+
+    handleUnsuccessfulAuth() {
+        this.props.handleSuccessfulLogin();
+    }
+
+    render() {
+        return (
+            <div className='auth-page-wrapper'>
+                <div
+                    className='left-column'
+                    style={{
+                        //backgroungImage: 'url(${loginImg})'
+                        backgroundColor: 'darkorange'
+                    }}
+                />
+                <div className='right-column'>
+                    <Login
+                        handleSuccessfulAuth={this.handleSuccessfulAuth}
+                        handleUnsuccessfulAuth={this.handleUnsuccessfulAuth}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,28 +1,29 @@
 import {
-    faTrash,
+    // faTrash,
     faSignOutAlt,
-    faEdit,
-    faSpinner,
-    faPlusCircle,
-    faPhone,
+    // faEdit,
+    // faSpinner,
+    // faPlusCircle,
+    // faPhone,
     faEnvelope,
-    faMapMarkedAlt,
+    // faMapMarkedAlt,
     faLock,
   } from "@fortawesome/free-solid-svg-icons";
   import { library } from "@fortawesome/fontawesome-svg-core";
   
   const Icons = () => {
     return library.add(
-      faTrash,
+    //   faTrash,
       faSignOutAlt,
-      faEdit,
-      faSpinner,
-      faPlusCircle,
-      faPhone,
+    //   faEdit,
+    //   faSpinner,
+    //   faPlusCircle,
+    //   faPhone,
       faEnvelope,
-      faMapMarkedAlt,
+    //   faMapMarkedAlt,
       faLock
     );
   };
   
   export default Icons;
+  

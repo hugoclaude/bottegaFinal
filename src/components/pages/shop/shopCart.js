082f88272e25da29e2a0a0ef2f9c3c6ab63a92dd
hugoclaude <hugocayeros@gmail.com>
2021-10-11 11:48:20 -0700

    created quantity component from shop

---
+++ src/components/pages/shop/shopCart.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import CartProduct from './cartProduct';
+
+import { connect } from 'react-redux';
+import * as actions from '../../../actions';
+
+import CartButton from './cartButton';
+
+import history from '../../../history';

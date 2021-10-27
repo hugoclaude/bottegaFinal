02b34b9f2c959c3a3d73458f2ead15fad824c97a
hugoclaude <hugocayeros@gmail.com>
2021-10-26 18:25:36 -0700

    completed missing files in checkout folder

--- src/components/pages/checkout/checkout.js
+++ src/components/pages/checkout/checkout.js
@@ -4,6 +4,38 @@ import { connect } from 'react-redux';
 import * as actions from '../../../actions';
 
 // import PageTitle from '../pageTitle';
-import checkoutForm from './checkoutForm';
+import CheckoutForm from './checkoutForm';
 
-class
+class Checkout extends Component {
+
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
+    }
+
+    onSubmit = (fields) => {
+        console.log('fields');
+    }
+
+    render() {
+        let subtotal= 0;
+        this.props.cartProducts.map(cartProduct => {
+            subtotal += cartProduct.quantity * cartProduct.product.price;
+        })
+        return (
+            <div className='checkout'>
+                <PageTitle className='checkout__page-title' title='Order Checkout'/>
+                <CheckoutForm className='checkout__form' onSubmit={this.onSubmit} subtotal={subtotal}/>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const { cartProducts } = state.user;
+    return { cartProducts }
+}
+
+Checkout = connect(mapStateToProps, actions)(Checkout);
+
+export default Checkout;
